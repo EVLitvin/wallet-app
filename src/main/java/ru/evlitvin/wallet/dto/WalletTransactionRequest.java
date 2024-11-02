8944a52d831c5bbd930a35cf0659d68f1b5daf97
LitvinEV <ev_litvin@yahoo.com>
2024-11-02 08:46:45 +0500

    Add all args constructor to WalletTransactionRequest.

--- src/main/java/ru/evlitvin/wallet/dto/WalletTransactionRequest.java
+++ src/main/java/ru/evlitvin/wallet/dto/WalletTransactionRequest.java
@@ -1,5 +1,6 @@
 package ru.evlitvin.wallet.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class WalletTransactionRequest {
 
   private UUID walletId;
